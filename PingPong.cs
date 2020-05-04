8f2d042fbf550403ee1aa07234ffa78ac1b9cf85
Tyler Bates <Fondlytyler@gmail.com>
2020-05-04 09:52:55 -0700

    successfully return designated strings upon met user input conditions.

--- PingPong.cs
+++ PingPong.cs
@@ -5,6 +5,29 @@ class PingPong
   static void Main()
 
   {
+    Console.WriteLine("Please enter a number.");
+    string stringUserInput = Console.ReadLine();
+    int inputNumber = int.Parse(stringUserInput);
 
+    for (int i = 1; i <= inputNumber; i++)
+    {
+      if (i % 3 == 0 && i % 5 == 0)
+      {
+        Console.WriteLine("Ping Pong");
+      }
+      else if (i % 5 == 0)
+      {
+        Console.WriteLine("Pong");
+      }
+      else if (i % 3 == 0)
+      {
+        Console.WriteLine("Ping");
+      }
+      else
+      {
+        Console.WriteLine(i);
+      }
+    }
   }
 }
+
